915d582d72091bf7db9cbb7963bee4da799aee46
KurosawaAkinori <b1919367@planet.kanazawa-it.ac.jp>
2021-03-21 19:34:32 +0900

    :bulb: add: comment

--- server/src/getMainichi.ts
+++ server/src/getMainichi.ts
@@ -1,54 +1,61 @@
 import puppeteer from 'puppeteer';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import Redis from 'ioredis';
 import { getElms } from './getElms';
 
-export const getMainichi = (req, res) => {
-  (async () => {
-    //measure time
-    const start = Date.now();
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+export const getMainichi = async () => {
+  //measure time
+  const start = Date.now();
+  //dayjs
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  const now = dayjs().tz('Asia/Tokyo').format('YYYY/MM/DD_HH:mm:ss');
+  console.log(`get Mainichi at ${now}`);
 
-    // block css,font
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-      if (
-        [
-          'stylesheet',
-          'font',
-          'media',
-          'script',
-          'xhr',
-          'fetch',
-          'websocket',
-          'eventsource',
-        ].includes(request.resourceType())
-      ) {
-        request.abort();
-      } else {
-        request.continue();
-      }
-    });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const titleSelector =
-      '[data-cx-area="top-selection"] .toppickup-detail > .toppickup-title.mb-10';
-    const titleSelector2 =
-      '[data-cx-area="top-selection"] ul.toppickuplist > li h3';
-    const anchorSelector = '[data-cx-area="top-selection"] .toppickup a';
-    const anchorSelector2 =
-      '[data-cx-area="top-selection"] ul.toppickuplist > li a';
-    const url = 'https://mainichi.jp/';
-    const newsArray = await getElms(page, url, titleSelector, anchorSelector);
-    const newsArray2 = await getElms(
-      page,
-      url,
-      titleSelector2,
-      anchorSelector2
-    );
-    const newsArray3 = [...newsArray, ...newsArray2];
-    // newsArray3.length = 7;
+  // block css,font
+  await page.setRequestInterception(true);
+  page.on('request', (request) => {
+    if (
+      [
+        'stylesheet',
+        'font',
+        'media',
+        'script',
+        'xhr',
+        'fetch',
+        'websocket',
+        'eventsource',
+      ].includes(request.resourceType())
+    ) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
 
-    await res.json(newsArray3);
-    await browser.close();
-    console.log('Took', Date.now() - start, 'ms');
-  })();
+  const titleSelector =
+    '[data-cx-area="top-selection"] .toppickup-detail > .toppickup-title.mb-10';
+  const titleSelector2 =
+    '[data-cx-area="top-selection"] ul.toppickuplist > li h3';
+  const anchorSelector = '[data-cx-area="top-selection"] .toppickup a';
+  const anchorSelector2 =
+    '[data-cx-area="top-selection"] ul.toppickuplist > li a';
+  const url = 'https://mainichi.jp/';
+  const newsArray = await getElms(page, url, titleSelector, anchorSelector);
+  const newsArray2 = await getElms(page, url, titleSelector2, anchorSelector2);
+  const newsArray3 = [...newsArray, ...newsArray2];
+  // newsArray3.length = 7;
+
+  //store json to redis
+  const redis = new Redis(process.env.REDIS_URL);
+  await redis.set('mainichi', JSON.stringify(newsArray3));
+
+  await browser.close();
+  redis.disconnect();
+  console.log('Took', Date.now() - start, 'ms (mainichi)');
 };
